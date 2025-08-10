c14a09d32af5eef5a31cb872d8ea029eaff3e45a
Saumitra Pathak <saumitra.pathak@hpe.com>
2025-08-10 23:01:27 +0530

    Bad day at work, only 1 middleware left

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -71,4 +71,18 @@ exports.loginUser = async (req, res) => {
   }
 };
 
-// exports.getUserProfile = async (req, res) => {};
+exports.getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({
+      message:
+        "Server error while fetching user profile, please try again later",
+      error: error.message,
+    });
+  }
+};
